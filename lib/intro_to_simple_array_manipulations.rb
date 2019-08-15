f429a3f35e8d7afd0c5c262e2bb0010e6e75fc40
Shamel Lakin <christlife4u@gmail.com>
2019-08-15 14:23:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,11 @@
 using_push = ["next_color", "green", "yellow", "red", "blue"]
 using_push.push("orange")
+p using_push
 
 
 using_unshift = [ "b", "c", "d" ]
 using_unshift.unshift("a")
+p using_unshift
 
 using_pop = ["africa", "asia", "north_america"]
 north_america = using_pop.pop
